b32c93375fade83902b6ea941dfaf1b64a9e34bc
rjpd7 <d.roeljoseph@gmail.com>
2025-09-13 02:01:35 +0800

    s

--- AccountingSystem/Accounting_System/urls.py
+++ AccountingSystem/Accounting_System/urls.py
@@ -2,10 +2,8 @@ from django.urls import path
 from . import views
 
 # name of the Django app.
-# This is used to refer to the app in templates and other parts of the Django project.
-app_name = "randomNumApp"
 
-# URL configuration for randomNumApp.
+# URL configuration for Accounting_System.
 urlpatterns = [
     path("", views.index, name="index"),
 ]
