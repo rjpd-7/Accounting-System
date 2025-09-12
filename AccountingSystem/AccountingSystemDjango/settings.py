6254dfc66632b590284448542497701f6965b51f
rjpd7 <d.roeljoseph@gmail.com>
2025-09-13 01:24:57 +0800

    edited settings in django admin

--- AccountingSystem/AccountingSystemDjango/settings.py
+++ AccountingSystem/AccountingSystemDjango/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Accounting_System',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
