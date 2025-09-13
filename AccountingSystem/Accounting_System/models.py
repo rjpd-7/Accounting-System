bc02ffa9a5ba4142d0a249f96eaf8479bcde3e5b
rjpd7 <d.roeljoseph@gmail.com>
2025-09-13 13:34:56 +0800

    editing database

--- AccountingSystem/Accounting_System/models.py
+++ AccountingSystem/Accounting_System/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class JournalEntry(models.Model):
+    date = models.DateField(default=date.today())
+    description = models.TextField(max_length=None, null=True)
+    account_name = models.TextField(max_length=None, null=True)
+    debit = models.DecimalField(decimal_places=10, max_digits=19, default=0)
+    credit = models.DecimalField(decimal_places=10, max_digits=19, default=0)
+
+    class Meta:
+        db_table = "sample"
