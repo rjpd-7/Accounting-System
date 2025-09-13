0e22fe57b1c134dbb5e5c1f3031abfe61a3a654d
rjpd7 <d.roeljoseph@gmail.com>
2025-09-13 11:51:26 +0800

    edited hover

--- AccountingSystem/AccountingSystem/urls.py
+++ AccountingSystem/AccountingSystem/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', include("Accounting_System.urls"))
+    path('index/', include("Accounting_System.urls")),
+    path('', include("Accounting_System.urls"))
 ]
