f32af03d27b769e986b4f389b4e2af6950387e1d
rjpd7 <d.roeljoseph@gmail.com>
2025-09-14 17:52:56 +0800

    added account table in accounts page

--- AccountingSystem/Accounting_System/views.py
+++ AccountingSystem/Accounting_System/views.py
@@ -33,4 +33,7 @@ def insert_journals(request):
     return render(request, "Front_End/journal.html")
 
 def chart_of_accounts(request):
-    return render(request, "Front_End/accounts.html")
+    results = JournalEntry.objects.all()
+    return render(request, "Front_End/accounts.html", {
+        "accounts" : results
+    })
